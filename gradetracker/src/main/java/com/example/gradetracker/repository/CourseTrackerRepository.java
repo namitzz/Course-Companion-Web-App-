ed088be91cf9ec9a81a0a50f865db126b10f1e47
ra495 <ra495@student.le.ac.uk>
2025-03-18 15:30:53 +0000

    Removed additional GradeTracker project

--- gradetracker/src/main/java/com/example/gradetracker/repository/CourseTrackerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.gradetracker.repository;
-
-import com.example.gradetracker.model.CourseTracker;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseTrackerRepository extends JpaRepository<CourseTracker, Long> {
-}
