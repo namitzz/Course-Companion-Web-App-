5ea140ac9b2ddc207e4cdb45fce3fc4fa8302198
ra495 <ra495@student.le.ac.uk>
2025-03-23 02:15:10 +0000

    Edited CSS for edit-profile.html

--- Project/src/main/java/com/example/project/repository/CourseTrackerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.project.repository;
-
-import com.example.project.model.CourseTracker;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseTrackerRepository extends JpaRepository<CourseTracker, Long> {
-}
